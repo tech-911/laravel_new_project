fc8ed9c7074f21ce46c24659b5507de64d855c1f
tech-911 <babatundeolatunji74@gmail.com>
2022-03-08 13:38:14 +0100

    adding some featueres

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,62 +1,197 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About</title>
- <style>
-        *{
-            box-sizing: border-box;
+    <title>Home</title>
+    <style>
+        /* ---------------General styles---------------- */
+        html,
+        body {
+            margin: 0;
+            height: 100vh;
         }
-      
-         .nav{
-             position: fixed;
-             right: 5%;
-             width: 100%;
+
+        /* ---------------Navbar styles---------------- */
+        .nav {
+            position: fixed;
+            top: 0px;
+            width: 100%;
+            background: rgba(33, 33, 33, 1);
+            z-index: 4;
+            padding: 20px;
+        }
+
+        .nav-child {
+
             display: flex;
-            list-style-type:none;
+            list-style-type: none;
             align-items: center;
             justify-content: flex-end;
-            
-         }
-         .nav > #li{
-            margin-right: 5%;
-         }
 
-        .nav > .font{
+
+        }
+
+        #li {
+            margin-right: 5%;
+        }
+
+        .nav>.font {
             font-family: Roboto;
             font-style: normal;
             font-weight: bold;
             font-size: 20px;
+
         }
 
-         a{
-             outline: none;
-             text-decoration: none;
-             color: black;
+        a {
+            outline: none;
+            text-decoration: none;
+            color: white;
 
-         }
+        }
+
+        /* ---------------body styles---------------- */
+
+        .body {
+            display: flex;
+            flex-direction: column;
+
+        }
+
+        .item1-about {
+            height: 100vh;
+            width: 100vw;
+            background-image: url({{ url('../assets/index.jpg') }});
+            background-repeat: no-repeat;
+            background-position: center;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .text1-about {
+            color: white;
+            font-weight: bold;
+            font-size: 80px;
+            font-family: Roboto;
+            border: 10px solid black;
+            padding: 40px;
+            text-shadow: 5px 6px 5px rgba(0, 0, 0, 0.25);
+        }
+
+        /* ---------------skillset styles---------------- */
+        .skillset {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            margin-top: 50px;
+        }
+
+        .sk1 {
+            border-bottom: 1px solid black;
+        }
+
+        .sk2 {
+            display: grid;
+            grid-template-columns: repeat(3, 1fr);
+            grid-gap: 1%;
+            width: 60%;
+
+        }
+
+        .about-img1 {
+            height: 300px;
+            width: 280px;
+        }
+
+        widget1 {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .span_about {
+            align: center
+        }
+
     </style>
-
 </head>
 
 <body>
-    <div class="about">
-        <ul class="nav">
-  <li id="li" class="font">
-  <a href="/home">Home</a>    
-  </li>
-  <li id="li" class="font">
-   <a href="/about">About</a>       
-  </li>
-  <li id="li" class="font">
-  <a href="/contact">Contact</a>   
-  </li>
-  <li class="font">
-  <a href="/reference">Reference</a>   
-  </li>
-</ul>
+    <div class="container">
+        <div class="nav">
+            <ul class="nav-child">
+                <li id="li" class="font">
+                    <a href="/home">Home</a>
+                </li>
+                <li id="li" class="font">
+                    <a href="/about">About</a>
+                </li>
+                <li id="li" class="font">
+                    <a href="/contact">Contact</a>
+                </li>
+                <li id="li" class="font">
+                    <a href="/reference">Reference</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="body">
+            <div class="item1-about">
+                <h1 class="text1-about">About Me</h1>
+            </div>
+            <div class="skillset">
+                <h1 class="sk1">Skillset</h1>
+                <div class="sk2">
+                    <div class="widget1">
+                        <img src="/assets/react.jpg" alt="noimage" class="about-img1">
+                        <h1>Web Development</h1>
+                        <span>Im into web development using React frame work. Also i aspire to extend my knowledge of
+                            web to backend with any backend technology. </span>
+                    </div>
+                    <div class="widget1">
+                        <img src="/assets/solidworks.jpg" alt="noimage" class="about-img1">
+                        <h1>Web Development</h1>
+                        <span>Im into web development using React frame work. Also i aspire to extend my knowledge of
+                            web to backend with any backend technology. </span>
+                    </div>
+                    <div class="widget1">
+                        <img src="/assets/figma.jpg" alt="noimage" class="about-img1">
+                        <h1>Web Development</h1>
+                        <span>Im into web development using React frame work. Also i aspire to extend my knowledge of
+                            web to backend with any backend technology. </span>
+                    </div>
+                    <div class="widget1">
+                        <img src="/assets/proteus.jpg" alt="noimage" class="about-img1">
+                        <h1>Web Development</h1>
+                        <span>Im into web development using React frame work. Also i aspire to extend my knowledge of
+                            web to backend with any backend technology. </span>
+                    </div>
+                    <div class="widget1">
+                        <img src="/assets/arduino.jpg" alt="noimage" class="about-img1">
+                        <h1>Web Development</h1>
+                        <span>Im into web development using React frame work. Also i aspire to extend my knowledge of
+                            web to backend with any backend technology. </span>
+                    </div>
+                    <div class="widget1">
+                        <img src="/assets/eagle.jpg" alt="noimage" class="about-img1">
+                        <h1>Web Development</h1>
+                        <span class="span_about">Im into web development using React frame work. Also i aspire to
+                            extend my knowledge of
+                            web to backend with any backend technology. </span>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+
     </div>
 </body>
+
 </html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -51,19 +51,56 @@
 
         }
 
-        .body {
+        .item1 {
+            height: 100vh;
             width: 100vw;
-            margin-top: 2.5%;
             background-image: url({{ url('../assets/index.jpg') }});
             background-repeat: no-repeat;
             background-position: center;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
 
+        .body {
+            display: flex;
+            flex-direction: column;
 
         }
 
-        .image {
-            width: 100vw;
-            height: 100vh;
+        .text1 {
+            color: white;
+            font-weight: bold;
+            font-size: 80px;
+            font-family: Roboto;
+            border: 10px solid black;
+            padding: 40px;
+            text-shadow: 5px 6px 5px rgba(0, 0, 0, 0.25);
+        }
+
+        .item2 {
+            display: flex;
+            align-items: flex-start;
+            justify-content: center;
+            color: black;
+            margin-top: 80px;
+
+        }
+
+        .img1 {
+
+            height: 499px;
+            width: 392px;
+
+        }
+
+        .item3 {
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+            margin-left: 2%;
+            font-size: 26px;
+
         }
 
     </style>
@@ -88,11 +125,31 @@
             </ul>
         </div>
 
-
         <div class="body">
-            <img src="{{ URL::to('assets/index.jpg') }}" alt="no image" class="image" />
-            jdsalkgj;lkfsjdaok flj;kl jadskljfd
+            <div class="item1">
+                <h1 class="text1">Babatunde Eric Olatunji</h1>
+            </div>
+            <div class="item2">
+
+                <img src="/assets/Profile_image.png" alt="noimage" class="img1">
+                <div class="item3">
+                    <h1 class="">Introduction</h1>
+                    <span>I am Babatunde Eric Olatunji a student of Mechatronics engineering Federal University of<br>
+                        Technology Minna. I worked with the Advnced Engineering Innovation Reaserch Group (AEIRG) as
+                        a<br>
+                        student Research Assistant.<br>
+                        My area of interest are:</span>
+                    <ul>
+                        <li>IoT</li>
+                        <li>web development</li>
+                        <li>Artificial intellegence</li>
+                        <li>3D design</li>
+                    </ul>
+                </div>
+
+            </div>
         </div>
+
 
     </div>
 </body>
